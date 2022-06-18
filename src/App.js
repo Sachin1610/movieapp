447c48eee25cf52c2ca207556b4659b847f80636
Sachin Patel <sachinp1610@gmail.com>
2022-06-18 23:31:28 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SearchMovies from "./components/SearchMovies/SearchMovies";
+import "./components/SearchMovies/SearchMovies.css";
+import movieimg from "./assets/film-reel.png";
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">
+        <img src={movieimg} alt="movie-reel" className="movieimg" /> Movie
+        Search App
+      </h1>
+      <SearchMovies />
+      <p style={{ textAlign: "center" }}>
+        &copy;2020 Created By Sachin Patel
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchMovies/SearchMovies.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Movie from "../MoviesComponent/Movies";
+const SearchMovies = () => {
+  // state - input query, movies
+  const [query, setQuery] = useState("");
+
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=c080cacaac9615d1c5e6cb5b4e8ae3b4&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results || []);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={searchMovies}>
+        <label htmlFor="query" className="label">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Marvel"
+          autoComplete="off"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Search!
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <Movie movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default SearchMovies;
